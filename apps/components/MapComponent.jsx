413f3be4370b32dbb82f5dc2a45117ec612e5401
Krithika Sarathy <krithikakameshwari06@gmail.com>
2026-05-13 23:27:42 +0530

    Update MapComponent.jsx

--- apps/components/MapComponent.jsx
+++ apps/components/MapComponent.jsx
@@ -4,7 +4,7 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 // Fix default marker icons
-delete L.Icon.Default.prototype._getIconUrl;
+delete L.Icon.Default.prototype._getIconUrl;  
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
   iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
