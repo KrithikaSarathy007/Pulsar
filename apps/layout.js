7fc334da2d10229f2549cf0c398a8d774934f1b0
Krithika Sarathy <krithikakameshwari06@gmail.com>
2026-05-13 23:28:16 +0530

    Update layout.js

--- apps/layout.js
+++ apps/layout.js
@@ -3,7 +3,7 @@ import { Toaster } from "sonner";
 
 export default function Layout({ children, currentPageName }) {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen"> 
       <Toaster position="top-right" richColors />
       {children}
     </div>
